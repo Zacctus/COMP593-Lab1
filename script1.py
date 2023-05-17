f38e8592898d9a2b71fde55f814584216c9c9d96
Zacctus <133024152+Zacctus@users.noreply.github.com>
2023-05-17 15:36:59 -0400

    removed + added print

--- script1.py
+++ script1.py
@@ -1,5 +1,5 @@
 # The Mind melting question
-print("Why do they call it chilli if it's hot?")
+
 
 # Satisfying conclusion to help the reader rationalize their confusion
 print("Must be a conspiracy...")
--- script2.py
+++ script2.py
@@ -3,3 +3,5 @@ print("When you purchase a bigger mattress, you both")
 
 # Zero-sum game in action
 print("gain and lose bedroom.")
+
+print("I really need to stop drinking")
